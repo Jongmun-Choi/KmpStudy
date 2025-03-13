3252d98b7dfd26da232bf32946c19987dfedfe8e
jongmun <jongmun.choi@illuminarean.com>
2025-03-13 12:48:30 +0900

    CommonMain쪽 Di, Network 모듈 셋팅

---
+++ shared/src/commonMain/kotlin/com/dave/kmpstudy/di/DataModule.kt
@@ -0,0 +1,8 @@
+package com.dave.kmpstudy.di
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+
+@Module
+@ComponentScan("com.dave.kmpstudy.data")
+class DataModule
